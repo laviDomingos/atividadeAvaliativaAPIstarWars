2b0ea545df8b1f1f0a0f5a67abc171d2a3657544
laviDomingos <lavinia_domingos@estudante.sesisenai.org.br>
2025-03-26 21:35:38 -0300

    Atualização API

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,6 @@
+import 'package:flutter/material.dart';
 import 'package:api_star_wars/models/people.dart';
 import 'package:api_star_wars/screens/detail_screen.dart';
-import 'package:flutter/material.dart';
 import 'package:api_star_wars/services/sw_api_service.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -14,12 +14,12 @@ class _HomeScreenState extends State<HomeScreen> {
   late Future<List<People>> _futurePeoples;
   final ApiService _apiService = ApiService();
 
-@override
+  @override
   void initState() {
     super.initState();
     _futurePeoples = _apiService.fetchPeoples();
   }
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,49 +27,49 @@ class _HomeScreenState extends State<HomeScreen> {
         title: const Text('Star Wars'),
         centerTitle: true,
       ),
-
-    body: FutureBuilder<List<People>>(
-      future: _futurePeoples,
-      builder: (context , snapshot) {
-        if(snapshot.connectionState == ConnectionState.waiting) {
-          return const Center(child: CircularProgressIndicator());
-        }else if(snapshot.hasError) {
-          return Center(child: Text('Erro: ${snapshot.error}'));
-        }else if(snapshot.hasData) {
-          final peoples = snapshot.data!;
-          return ListView.builder(
-            itemCount: peoples.length,
-            itemBuilder: (context, index) {
-              final people = peoples[index];
-              return GestureDetector(
-                child: Card(
-                    margin: const EdgeInsets.symmetric(
-                      horizontal: 16, vertical: 8),
-                      elevation: 4,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(16)),
-                        child: ListTile(
-                            onTap: () {
-                              print("Clicou no personagem: ${people.name}");
-                              Navigator.push(
-                                context, 
-                                MaterialPageRoute(
-                                  builder: (context) =>
-                                    DetailScreen(people),
-                                ),
-                              );
-                            },
-                            leading: Placeholder(),                     
-                              title: Text(people.name),
-                              subtitle: Text('Homeword: ${people.homeworld}'),
-                            )));
-            },       
-          );
-        } else {
-            return Placeholder();
-        }
-      }
-    )
+      body: FutureBuilder<List<People>>(
+        future: _futurePeoples,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Erro: ${snapshot.error}'));
+          } else if (snapshot.hasData) {
+            final peoples = snapshot.data!;
+            return ListView.builder(
+              itemCount: peoples.length,
+              itemBuilder: (context, index) {
+                final people = peoples[index];
+                return GestureDetector(
+                  child: Card(
+                    margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                    elevation: 4,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    child: ListTile(
+                      onTap: () {
+                        print("Clicou no personagem: ${people.name}");
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => DetailScreen(people),
+                          ),
+                        );
+                      },
+                      leading: Icon(Icons.person),
+                      title: Text(people.name),
+                      subtitle: Text('Gênero: ${people.gender}'), 
+                    ),
+                  ),
+                );
+              },
+            );
+          } else {
+            return Center(child: Text('Nenhum personagem encontrado.'));
+          }
+        },
+      ),
     );
   }
 }
